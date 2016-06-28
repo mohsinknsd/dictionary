f49f12b000f33276cefd2565f2aa5f5b1aff0616
“khansquare” <khan.square@gmail.com>
2016-06-28 11:32:06 +0530

    Token Security Added

--- src/main/java/com/square/dictionary/config/TokenFilter.java
+++ src/main/java/com/square/dictionary/config/TokenFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class TokenFilter implements Filter{
@@ -16,20 +17,33 @@ public class TokenFilter implements Filter{
 		// TODO Auto-generated method stub		
 	}
 
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
-			throws IOException, ServletException {
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-        chain.doFilter(req, res);		
-		 //token security
+             	
+        HttpServletRequest request = (HttpServletRequest) req;
+        String resource = request.getRequestURI().substring(1);
+        System.out.println(resource);
+        //If request is for token or web application then allow otherwise check token for apis in else block
+        if (resource.equals("/dictionary/auth/token") || resource.equals("/dictionary/app")) {
+        	chain.doFilter(req, res);        	
+        } else {
+        	try {
+        		if (req.getParameter("token") != null || req.getParameter("token").equals("")) {
+            		chain.doFilter(req, res);            		
+            	} else {            		
+            		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "You are not authorized for the request!");
+            	}	
+        	} catch (NullPointerException e) {        		
+        		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "You are not authorized for the request!");
+        	}
+        }        
 	}
 
 	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
+		// TODO Auto-generated method stub		
 	}
-
 }
